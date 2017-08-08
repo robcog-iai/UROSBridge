d49f8af2ebdae90ff349f6ff20979d00d0693c12
Andrei Haidu <andrei.haidu@yahoo.com>
2017-08-08 08:18:04 +0200

    plugin template

---
+++ Source/UROSBridge/Private/UROSBridge.cpp
@@ -0,0 +1,20 @@
+// Copyright 2017, Institute for Artificial Intelligence - University of Bremen
+
+#include "UROSBridge.h"
+
+#define LOCTEXT_NAMESPACE "FUROSBridgeModule"
+
+void FUROSBridgeModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FUROSBridgeModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUROSBridgeModule, UROSBridge)
