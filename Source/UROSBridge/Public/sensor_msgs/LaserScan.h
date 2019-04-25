59ddade871520bfa34c758ba9d4545d3b65e54c9
Michael Neumann <neumann.michael@uni-bremen.de>
2019-04-25 09:22:12 +0200

    add LaserScan msg

--- Source/UROSBridge/Public/sensor_msgs/LaserScan.h
+++ Source/UROSBridge/Public/sensor_msgs/LaserScan.h
@@ -1,6 +1,7 @@
-// Copyright 2018 Kannan Thambiah. All rights reserved.
 #pragma once
+
 #include "ROSBridgeMsg.h"
+
 #include "std_msgs/Header.h"
 
 namespace sensor_msgs
@@ -17,34 +18,41 @@ namespace sensor_msgs
 		float RangeMax;
 		TArray<float> Ranges;
 		TArray<float> Intensities;
-
-		public:
+	public:
 		LaserScan()
 		{
 			MsgType = "sensor_msgs/LaserScan";
 		}
 
 		LaserScan
-			(std_msgs::Header InHeader, float InAngleMin, float InAngleMax, float InAngleIncrement, float InTimeIncrement,
-			 float InScanTime, float InRangeMin, float InRangeMax, TArray<float> const &InRanges, TArray<float> const &InIntensities)
-			:
-				Header(InHeader),
-				AngleMin(InAngleMin),
-				AngleMax(InAngleMax),
-				AngleIncrement(InAngleIncrement),
-				TimeIncrement(InTimeIncrement),
-				ScanTime(InScanTime),
-				RangeMin(InRangeMin),
-				RangeMax(InRangeMax),
-				Ranges(InRanges),
-				Intensities(InIntensities)
+		(
+			std_msgs::Header InHeader,
+			float InAngleMin,
+			float InAngleMax,
+			float InAngleIncrement,
+			float InTimeIncrement,
+			float InScanTime,
+			float InRangeMin,
+			float InRangeMax,
+			const TArray<float>& InRanges,
+			const TArray<float>& InIntensities
+		):
+			Header(InHeader),
+			AngleMin(InAngleMin),
+			AngleMax(InAngleMax),
+			AngleIncrement(InAngleIncrement),
+			TimeIncrement(InTimeIncrement),
+			ScanTime(InScanTime),
+			RangeMin(InRangeMin),
+			RangeMax(InRangeMax),
+			Ranges(InRanges),
+			Intensities(InIntensities)
 		{
 			MsgType = "sensor_msgs/LaserScan";
 		}
 
 		~LaserScan() override {}
 
-		//--- Getters ---//
 		std_msgs::Header GetHeader() const
 		{
 			return Header;
@@ -95,7 +103,6 @@ namespace sensor_msgs
 			return Intensities;
 		}
 
-		//--- Setters ---//
 		void SetHeader(std_msgs::Header InHeader)
 		{
 			Header = InHeader;
@@ -136,46 +143,46 @@ namespace sensor_msgs
 			RangeMax = InRangeMax;
 		}
 
-		void SetRanges(TArray<float> const &InRanges)
+		void SetRanges(TArray<float>& InRanges)
 		{
 			Ranges = InRanges;
 		}
 
-		void SetIntensities(TArray<float> const &InIntensities)
+		void SetIntensities(TArray<float>& InIntensities)
 		{
 			Intensities = InIntensities;
 		}
 
-		//--- methods ---//
 		virtual void FromJson(TSharedPtr<FJsonObject> JsonObject) override
 		{
-			// Scalar values
 			Header = std_msgs::Header::GetFromJson(JsonObject->GetObjectField(TEXT("header")));
 
 			AngleMin = JsonObject->GetNumberField(TEXT("angle_min"));
+
 			AngleMax = JsonObject->GetNumberField(TEXT("angle_max"));
+
 			AngleIncrement = JsonObject->GetNumberField(TEXT("angle_increment"));
+
 			TimeIncrement = JsonObject->GetNumberField(TEXT("time_increment"));
+
 			ScanTime = JsonObject->GetNumberField(TEXT("scan_time"));
+
 			RangeMin = JsonObject->GetNumberField(TEXT("range_min"));
+
 			RangeMax = JsonObject->GetNumberField(TEXT("range_max"));
 
-			// Arrays
 			TArray<TSharedPtr<FJsonValue>> ValuesPtrArr;
 
 			Ranges.Empty();
 			ValuesPtrArr = JsonObject->GetArrayField(TEXT("ranges"));
 			for (auto &ptr : ValuesPtrArr)
-			{
 				Ranges.Add(ptr->AsNumber());
-			}
 
 			Intensities.Empty();
 			ValuesPtrArr = JsonObject->GetArrayField(TEXT("intensities"));
 			for (auto &ptr : ValuesPtrArr)
-			{
 				Intensities.Add(ptr->AsNumber());
-			}
+
 		}
 
 		static LaserScan GetFromJson(TSharedPtr<FJsonObject> JsonObject)
@@ -185,51 +192,10 @@ namespace sensor_msgs
 			return Result;
 		}
 
-		virtual FString ToString() const override
-		{
-			// Construct Array strings
-			FString RangesString = "[ ";
-			for (auto &value : Ranges)
-			{
-				RangesString += FString::SanitizeFloat(value) + TEXT(", ");
-			}
-			RangesString += "] ";
-
-			FString IntensitiesString = "[ ";
-			for (auto &value : Intensities)
-			{
-				IntensitiesString += FString::SanitizeFloat(value) + TEXT(", ");
-			}
-
-			return TEXT("LaserScan { header = ") + Header.ToString() +
-				TEXT(", angle_min = ") + FString::FromInt(AngleMin) +
-				TEXT(", angle_max = ") + FString::FromInt(AngleMax) +
-				TEXT(", angle_increment = ") + FString::FromInt(AngleIncrement) +
-				TEXT(", time_increment = ") + FString::FromInt(TimeIncrement) +
-				TEXT(", scan_time = ") + FString::FromInt(ScanTime) +
-				TEXT(", range_min = ") + FString::FromInt(RangeMin) +
-				TEXT(", range_max = ") + FString::FromInt(RangeMax) +
-				TEXT(", ranges = ") + RangesString  +
-				TEXT(", intensities = ") + IntensitiesString +
-				TEXT(" } ");
-		}
-
 		virtual TSharedPtr<FJsonObject> ToJsonObject() const override
 		{
 			TSharedPtr<FJsonObject> Object = MakeShareable<FJsonObject>(new FJsonObject());
 
-			// Construct Arrays
-			TArray<TSharedPtr<FJsonValue>> RangesArray, IntensitiesArray;
-			for (auto &range : Ranges)
-			{
-				RangesArray.Add(MakeShareable(new FJsonValueNumber(range)));
-			}
-			for (auto &intensity : Intensities)
-			{
-				IntensitiesArray.Add(MakeShareable(new FJsonValueNumber(intensity)));
-			}
-
-			// Construct JSON
 			Object->SetObjectField(TEXT("header"), Header.ToJsonObject());
 			Object->SetNumberField(TEXT("angle_min"), AngleMin);
 			Object->SetNumberField(TEXT("angle_max"), AngleMax);
@@ -238,12 +204,16 @@ namespace sensor_msgs
 			Object->SetNumberField(TEXT("scan_time"), ScanTime);
 			Object->SetNumberField(TEXT("range_min"), RangeMin);
 			Object->SetNumberField(TEXT("range_max"), RangeMax);
+			TArray<TSharedPtr<FJsonValue>> RangesArray;
+			for (auto &val : Ranges)
+				RangesArray.Add(MakeShareable(new FJsonValueNumber(val)));
 			Object->SetArrayField(TEXT("ranges"), RangesArray);
+			TArray<TSharedPtr<FJsonValue>> IntensitiesArray;
+			for (auto &val : Intensities)
+				IntensitiesArray.Add(MakeShareable(new FJsonValueNumber(val)));
 			Object->SetArrayField(TEXT("intensities"), IntensitiesArray);
-
 			return Object;
 		}
-
 		virtual FString ToYamlString() const override
 		{
 			FString OutputString;
@@ -252,4 +222,4 @@ namespace sensor_msgs
 			return OutputString;
 		}
 	};
-} // namespace sensor_msgs
+}
